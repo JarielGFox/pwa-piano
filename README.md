cb4d97b068f0ca5cba22d40a577975ab1cc559ce
Giovanni <deusexmachinak2@gmail.com>
2022-07-24 19:02:24 +0200

    test1

---
+++ README.md
@@ -0,0 +1,18 @@
+- Synth Piano source code <br>
+
+<br>
+
+- Idea:
+    > Boolean Coding Week
+    
+<br>
+    
+- Sviluppatore:
+    > Giovanni Crudele
+       
+<br>
+
+- Linguaggi usati:
+    >   JS ECMA 2020 <br>
+        HTML 5.0 <br>
+        CSS 3.0 <br>
--- script.js
+++ script.js
@@ -1,28 +1,72 @@
-const keyElements = document.querySelectorAll('.key');
+const pianoKeysElements = document.querySelectorAll('.key');
 
-function playSound() {
-    console.log('eseguo suono');
-    const audioElement = new Audio(); 
-    audioElement.src = 'sounds/01-do.mp3'; 
-    audioElement.play(); 
+let interactionEvent;
+if('ontouchend' in document.documentElement) {
+    interactionEvent = 'touchend'; 
+ } else {
+    interactionEvent = 'click';
+ }
+
+const notes = {
+    do: '01-do.mp3',
+    dodiesis: '02-dodiesis.mp3',
+    re: '03-re.mp3',
+    rediesis: '04-rediesis.mp3',
+    mi: '05-mi.mp3',
+    fa: '06-fa.mp3',
+    fadiesis: '07-fadiesis.mp3',
+    sol: '08-sol.mp3',
+    soldiesis: '09-soldiesis.mp3',
+    la: '10-la.mp3',
+    ladiesis: '11-ladiesis.mp3',
+    si: '12-si.mp3'
+};
+
+// Frequencies list: http://www.phy.mtu.edu 
+const frequencies = {
+    do: 233.08,
+    dodiesis: 261.63,
+    re: 277.18,
+    rediesis: 293.66,
+    mi: 311.13,
+    fa: 329.63,
+    fadiesis: 349.23,
+    sol: 369.9,
+    soldiesis: 392.00,
+    la: 415.30,
+    ladiesis: 440.00,
+    si: 466.16
+};
+
+function playNote(key) {
+    console.log('Playing: ', key);
+    const note = notes[key]
+    const audioElement = new Audio('./sounds/' + note);
+    audioElement.currentTime = 0;
+    audioElement.play();
 }
 
-keyElements.forEach(function(keyElement) {
-    keyElement.addEventListener('click', function() { 
-        playSound();
-    })
-  
-    keyElement.addEventListener('touchend', function() { 
-        playSound();
-    })
+function playSynth(key) {
+    const ctx = new window.AudioContext();
+    const oscillator = ctx.createOscillator();
+    const note = frequencies[key];
+    const length = 200;
+    
+    gainNode = ctx.createGain();
+    oscillator.type = 'triangle';
+    oscillator.frequency.value = note;
+    oscillator.connect(gainNode);
+    gainNode.connect(ctx.destination);
+    oscillator.start(0);
+    oscillator.stop(ctx.currentTime + (length / 1000 + 0.2));
+    oscillator.onended = () => ctx.close();
+}
+
+pianoKeysElements.forEach(function(keyElement) {
+    keyElement.addEventListener(interactionEvent, function () {
+        const key = keyElement.id;
+        playSynth(key);
+    });
 });
 
-const num1 = 1;
-const num2 = 2;
-
-function miaFunzione(num1, num2) {
-    return num1 + num2 ;
-}
-
-const result = miaFunzione(1, 2)
 
